cb7eb7319aaf990fff76b045c5475bbdadf8650f
BruceLee <axengine@163.com>
2020-08-10 14:31:03 +0800

    fix signal notify.the same with go-micro

--- leaf.go
+++ leaf.go
@@ -8,6 +8,7 @@ import (
 	"github.com/axengine/leaf/module"
 	"os"
 	"os/signal"
+	"syscall"
 )
 
 func Run(mods ...module.Module) {
@@ -37,7 +38,8 @@ func Run(mods ...module.Module) {
 
 	// close
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, os.Kill)
+	signal.Notify(c, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	//signal.Notify(c, os.Interrupt, os.Kill)
 	sig := <-c
 	log.Release("Leaf closing down (signal: %v)", sig)
 	console.Destroy()
